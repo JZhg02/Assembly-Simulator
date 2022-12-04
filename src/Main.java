efd2e2337b3e3fed43af89ddc6bc921462d51b24
JZhg02 <94388221+JZhg02@users.noreply.github.com>
2022-12-04 10:14:22 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,6 @@ public class Main {
         System.out.println("Instructions in the map: "+myfile.myInstructions);
         myfile.getMemory();
         System.out.println("Memory: "+myfile.tempMemory);
-        System.out.println("TEST TEST ");
+        System.out.println("Testing");
     }
 }
